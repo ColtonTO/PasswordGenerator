9ebd251234c39337331b856aaeebf359e2977a3a
ColtonTO <colton1105@gmail.com>
2022-11-23 00:58:17 -0800

    prompts work in DevTools

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,41 @@
+var characterLength = 8;
+var choiceArr = [];
+
+var specialCharArr= ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')','?','~','+','-','_','|','/',]
+var lowerCaseArr = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m',]
+var upperCaseArr = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M',]
+var numberArr = ['0','1','2','3','4','5','6','7','8','9',]
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword () {
+  console.log("Button is Clicked");
+}
+
+function getPrompts() {
+  choiceArr = [];
+  characterLength = prompt("How many characters do you want in your password? (8-128 characters)");
+
+  if(isNaN(characterLength) || characterLength <8 || characterLength > 128 ) {
+    alert("Character length MUST be a number, 8-128 characters only.")
+    return false;
+  }
+  if (confirm("Would you like lowercase letters in this password?")) {
+    choiceArr = choiceArr.concat(lowerCaseArr);
+  }
+  if (confirm("Would you like uppercase letters in this password?")) {
+      choiceArr = choiceArr.concat(upperCaseArr);
+  }
+  if (confirm("Would you like special characters in this password?")) {
+      choiceArr = choiceArr.concat(specialCharArr);
+  }
+  if (confirm("Would you like numbers in this password?")) {
+    choiceArr = choiceArr.concat(numberArr);
+  }
+  return true;
+}
+
 
 // Write password to the #password input
 function writePassword() {
